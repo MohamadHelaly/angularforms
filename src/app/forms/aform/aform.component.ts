35737b6b4ee92bb0a21e8163de7353f1d5ed9f5f
MohamadHelalyLabtop <mohamad.helaly@gmail.com>
2021-07-03 17:08:43 +0300

    Create Form / basic html with b5 classes

--- src/app/forms/aform/aform.component.ts
+++ src/app/forms/aform/aform.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AformComponent implements OnInit {
 
+  topics: string[] = ['Angular', 'React', 'Vue'];
   constructor() { }
 
   ngOnInit(): void {
